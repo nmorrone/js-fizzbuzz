737922921c573e0ae97fec181beb47df57bcd561
nmorrone <n.morrone96@gmail.com>
2024-07-22 11:09:29 +0200

    js-for-100

---
+++ JS/app.js
@@ -0,0 +1,15 @@
+/* Esercizio FizzBuzz
+stampare in console n da 1 a 100
+multipli 3 = Fizz
+multipli 5 = 5
+multipli 3e5 = FizzBuzz */
+
+//apro il ciclo con le condizioni
+//assegno una variabile che incrementerà fino al numero 100
+
+for (let counter = 1; counter <= 100; counter++) {
+
+    //stampo in console
+    console.log(counter)
+
+}
